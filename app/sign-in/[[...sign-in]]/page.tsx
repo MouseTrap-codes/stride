1921248ed6d98b3b104312fae19577a5982ec93e
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-12-26 21:20:16 -0600

    clerk auth works

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -7,11 +7,6 @@ export default function Page() {
     <div className="relative min-h-screen flex items-center justify-center bg-stride-bg px-4">
       <SignIn 
         appearance={{
-            elements: {
-                // card: "bg-zinc-900 border border-zinc-800 shadow-2xl",
-                // formButtonPrimary:
-                // "bg-stride-blue hover:bg-stride-blue-hover text-white font-medium",
-            },
             variables: {
                 colorPrimary: "var(--color-stride-blue)",
             },
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -6,11 +6,7 @@ export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-stride-bg px-4">
       <SignUp 
-        appearance={{
-                elements: {
-                    formButtonPrimary:
-                    "bg-stride-blue hover:bg-stride-blue-hover text-white font-medium",
-                },
+            appearance={{
                 variables: {
                     colorPrimary: "var(--color-stride-blue)",
                 },
