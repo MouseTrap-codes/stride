83b96fa55e4cf22fa65f0c268286edf80c4a611b
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-12-27 18:56:19 -0600

    create GET and PUT for projects

---
+++ app/api/projects/route.ts
@@ -0,0 +1,64 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server"
+import { prisma } from "@/lib/prisma";
+import { createProjectSchema } from "@/lib/validators";
+import { decodeAction } from "next/dist/server/app-render/entry-base";
+
+
+// GET
+export async function GET() {
+    const { userId } = await auth();
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401});
+    }
+
+    const projects = await prisma.project.findMany({
+        where: { userId },
+        orderBy: { createdAt: "desc"},
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true,
+            _count: {select: {tasks:true}}
+        }
+    });
+
+    return NextResponse.json({ data: projects }, { status:200 });
+}
+
+// POST
+export async function POST(req: Request) {
+    const { userId } = await auth();
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401});
+    }
+
+    const body = await req.json().catch(() => null);
+    const parsed = createProjectSchema.safeParse(body);
+
+    if (!parsed.success) {
+        return NextResponse.json(
+            { error: "Invalid body", details: parsed.error.flatten() },
+            { status: 400}
+        );
+    }
+
+    const project = await prisma.project.create({
+        data: {
+            userId,
+            name: parsed.data.name,
+            description: parsed.data.description,
+        },
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true,
+        }
+    });
+
+    return NextResponse.json({ data: project }, { status: 201 });
+}
